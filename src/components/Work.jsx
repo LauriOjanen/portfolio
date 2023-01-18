2fb4519bc494f09e0400727d311337fc66cf85a9
LauriOjanen <78724537+LauriOjanen@users.noreply.github.com>
2023-01-18 21:33:30 +0200

    Creare skills

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import workImg from '../assets/workImg.jpeg'
+import realEstate from '../assets/realestate.jpg'
 
 const Work = () => {
   return (
@@ -6,21 +8,22 @@ const Work = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-                <p>// Check out some of my recent work</p>
+                <p className='py-6'>// Check out some of my recent work</p>
             </div>
 
-            <div>
-                <div>
-                    <div>
-                        <span>
-
+            <div style={{backgroundImage: `url(${workImg})`}}
+            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React JS Application
                         </span>
-                        <div>
+                        <div className='pt-8 text-center'>
                             <a href="/">
-                                <button></button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href="/">
-                                <button></button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
 
                         </div>
